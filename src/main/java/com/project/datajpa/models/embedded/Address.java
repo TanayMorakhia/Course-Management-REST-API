5169e97e9270a3b80df0fa080c317cc6e099bf77
Tanay <tdmorakhia26@gmail.com>
2024-06-22 15:58:10 +0530

    added embeddable ids and entites

---
+++ src/main/java/com/project/datajpa/models/embedded/Address.java
@@ -0,0 +1,17 @@
+package com.project.datajpa.models.embedded;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Address {
+
+    private String houseNo;
+
+    private String street;
+}
---
+++ src/main/java/com/project/datajpa/models/embedded/OrderId.java
@@ -0,0 +1,20 @@
+package com.project.datajpa.models.embedded;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class OrderId implements Serializable {
+
+    private String username;
+
+    private LocalDateTime orderData;
+}
